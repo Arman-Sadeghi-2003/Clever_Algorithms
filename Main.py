9c131580d47d0a5292b91cacfff49dc2a0cffb68
Arman Sadeghi <Terminator_arman@yahoo.com>
2025-10-04 08:49:34 +0330

    Added ILS algorithm

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from Random_search.Adaptive_random_search import AdaptiveRandomSearch as ARS
 from Random_search.Random_Search import RandomSearch as RS
+from Random_search.Iterated_local_search import IteratedLocalSearch as ILS
 
 # Sphere function minimization
 def sphere(v):
@@ -7,6 +8,8 @@ def sphere(v):
 
 
 # Random search sample usage
+
+print("RS:")
 
 bounds = [(-5, 5), (-5, 5)]
 res = RS.random_search(sphere, bounds, iterations=1000, seed=42)
@@ -18,6 +21,8 @@ print("\n\n_____________________\n\n")
 
 # Adaptive random search sample usage
 
+print("ARS:")
+
 bounds = [(-5, 5), (-5, 5)]
 ars = ARS(sphere, bounds, step_size=0.1, iterations=2000, maximize=False, seed=42)
 result = ars.run()
@@ -25,3 +30,23 @@ result = ars.run()
 print("Best solution:", result["best_x"])
 print("Best score:", result["best_score"])
 
+
+print("\n\n_____________________\n\n")
+
+# Iterated local search usage sample
+
+print("ILS:")
+
+bounds = [(-5, 5), (-5, 5)]
+
+ils = ILS(sphere, bounds,
+          step_size=0.05,
+          perturb_strength=0.5,
+          iterations=1000,
+          maximize=False,
+          seed=42)
+
+result = ils.run()
+
+print("Best solution:", result["best_x"])
+print("Best score:", result["best_score"])
